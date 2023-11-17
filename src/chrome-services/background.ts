3b76aaa2d0adba8591772edd010af1e6134fc7bb
JStuve <stuve.josh@gmail.com>
2023-11-16 20:25:53 -0700

    Wrapped chrome api so react app can be served

--- src/chrome-services/background.ts
+++ src/chrome-services/background.ts
@@ -1,9 +1,12 @@
 import { Message, MessageType } from "../models";
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
-	if(changeInfo.status === 'complete') {
-		chrome.tabs.sendMessage<Message<null>>(tabId, { type: MessageType.IssueLoadUI, data: null })
-	}
-})
+if(chrome?.tabs?.onUpdated) {
+	chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+		if(changeInfo.status === 'complete') {
+			chrome.tabs.sendMessage<Message<null>>(tabId, { type: MessageType.IssueLoadUI, data: null })
+		}
+	})
+}
+
 
 export {};
